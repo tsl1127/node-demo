400272ad29ab93446aa10b853373dd1ff245a5e6
tsl <taosiliang1127tsl@163.com>
2018-01-26 14:55:41 +0800

    增加index.html  index.js  main.js style.css

---
+++ index.js
@@ -0,0 +1,44 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+//console.log(Object.keys(http))
+var port = process.env.PORT || 8888;
+
+var server = http.createServer(function(request, response){
+
+  var temp = url.parse(request.url, true)
+  var path = temp.pathname
+  var query = temp.query
+  var method = request.method
+
+  //从这里开始看，上面不要看
+
+  if(path === '/'){  // 如果用户请求的是 / 路径
+    var string = fs.readFileSync('./index.html','utf8')  // 就读取 index.html 的内容
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
+    response.write(string)
+    response.end()   // 设置响应消息体
+  }else if(path === '/style.css'){   // 如果用户请求的是 /style.css 路径
+    var string = fs.readFileSync('./style.css','utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){  // 如果用户请求的是 /main.js 路径
+    var string = fs.readFileSync('./main.js','utf8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(string)
+    response.end()
+  }else{  // 如果上面都不是用户请求的路径
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')  // 设置响应头 Content-Type
+    response.write('找不到对应的路径，你需要自行修改 index.js')
+    response.end()
+  }
+
+  // 代码结束，下面不要看
+  console.log(method + ' ' + request.url)
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功，请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
--- server.js
+++ server.js
@@ -10,59 +10,31 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-
-
-
-
-
-
-
-
-
-
-
-  console.log('HTTP 路径：为\n' + path)
-  if(path == '/style'){
-  response.setHeader('Content-Type','text/css;charset=utf-8')
-  response.write('body{background-color: #ddd;}h1{color: red;}')
-  response.end()
-  }else if(path == '/main'){ 
-  response.setHeader('Content-Type','text/javascript;charset=utf-8')
-  response.write('alert("这是JS执行的")')
-  response.end()
-  }else if(path == '/'){
-  response.setHeader('Content-Type','text/html; charset=utf-8')
-  response.write('<!DOCTYPE>\n<html>'+
-	  '<head><link rel="stylesheet" href="/style">'+
-	  '</head><body>'+
-	  '<h1>你好</h1>'+
-	  '<script src="/main"></script>'+
-	  '</body></html>')
-  response.end()
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
+    response.end()
   }else{
-  response.statusCode = 404
-  response.end()
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
+    response.end()
   }
-
-
-
-
-
 
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
